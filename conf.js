27a96131a0e083be75b77706090326713cc4a810
vildozo <vildozo@outlook.com>
2017-11-10 23:47:53 -0400

    Actualizando pruebas, no funciona la NUMERO 4

--- conf.js
+++ conf.js
@@ -1,6 +1,20 @@
-// conf.js
+//
+//exports.config = {
+//  directConnect: true,//runs selenium webdriver y el browser por defecto solo para chrome y firefox
+//  framework: 'jasmine',
+//  //seleniumAddress: 'http://localhost:4444/wd/hub', //Se comenta porque se puso directConnect:true y ya no se necesita
+//  specs: ['firstTest.js'],
+//  
+////Options to be passed to Jasmine.
+//  jasmineNodeOpts: {
+//    defaultTimeoutInterval: 30000 //time to kill test 30 seconds
+//  }
+//
+//}
+
 exports.config = {
   framework: 'jasmine',
+  directConnect: true,
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['spec.js']
 }
--- spec.js
+++ spec.js
@@ -1,15 +1,9 @@
 
 describe('Protractor Children Management', function() {
-/*
-  var firstNumber = element(by.model('first'));
-  var secondNumber = element(by.model('second'));
-  var goButton = element(by.id('gobutton'));
-  var latestResult = element(by.binding('latest'));
-*/
-  var header0Childs = element(by.id('NoChildRegistered'));
-  //var addAChild = element(by.css("watchlist_menu button button-small button-clear button-positive"));
+
   var factoryNewChild = element(by.css('title title-left'));
   var addAChild= element(by.id('new-child-btn'));
+  var childsName=element(by.id('childs_name'));
 
 
 
@@ -17,44 +11,76 @@ describe('Protractor Children Management', function() {
     browser.get('http://localhost:8100');
   });
 
-    it('should have a title', function() {
-     expect(browser.getTitle()).toEqual('Manage Children');
-   });
+  //       //TEST 1
+  //   it('should have a title', function() {
+  //    expect(browser.getTitle()).toEqual('Manage Children');
+  //  });
+  //
+  //       //TEST 2
+  // it('No Child registered', function() {
+  //   expect(element(by.id('NoChildRegistered')).getText()).toEqual("PLEASE REGISTER ONE ABOVE");
+  // });
+
+        //TEST 3
+
+    // it('Open New Child modal', function() {
+    //     addAChild.click();
+    //     titleModalNewChild = element(by.id('head_New_Child'));
+    //     expect(titleModalNewChild.getText()).toEqual("New Child");
+    // });
+
+        //TEST 4 FALLA ESTA PRUEBA
+  // it('Add a Child', function() {
+  //   addAChild.click();
+  //   var childsNameInput = element(by.id('childs_name'));
+  //   element(by.model("child.first_name")).sendKeys("Javier Vildozo");
+  //   element(by.model("child.birthday")).sendKeys("06/22/1981");
+  //   element(by.model("child.gender")).element(by.css("[value='Male']")).click();
+  //   element(by.buttonText("Create")).click();
+  //   browser.sleep(2000);
+  //   element.all(by.id("nombreNino")).getText().then(function (text) {
+  //       console.log(text);
+  //   })
+  //   browser.sleep(10000);
+  // });
+
+        //TEST 5
+    // it('Cant create a child with the name field empty', function() {
+    //     addAChild.click();
+    //     var childsNameInput = element(by.id('childs_name'));
+    //     element(by.model("child.gender")).element(by.css("[value='Male']")).click();
+    //     expect(element(by.id('head_New_Child')).getText()).toEqual("New Child");
+    //
+    //     browser.sleep(1000);
+    // });
+
+    //     //TEST 6
+    // it('Registrar niño con fecha posterior a la actual', function() {
+    //   addAChild.click();
+    //   var childsNameInput = element(by.id('childs_name'));
+    //   element(by.model("child.first_name")).sendKeys("Javier Vildozo");
+    //   element(by.model("child.gender")).element(by.css("[value='Male']")).click();
+    //   element(by.model("child.birthday")).sendKeys("06/22/2081");
+    //   element(by.buttonText("Create")).click();
+    //     this.popupContainsHeaderText = function (text) {
+    //         this.popupShouldExist();
+    //         expect(this.popup.element(by.css('.popup-head')).getText()).toMatch("Please insert a date prior to today");
+    //     };
+    //   browser.sleep(4000);
+    // });
 
 
-  it('No Child registered', function() {
-    expect(header0Childs.getText()).toEqual("PLEASE REGISTER ONE ABOVE");
-  });
-
-
-  it('Open New Child modal', function() {
-    addAChild.click();
-    titleModalNewChild= element(by.id('head_New_Child'));
-   expect(titleModalNewChild.getText()).toEqual("New Child");
-  });
-
-  it('Add a Child', function() {
-    addAChild.click();
-    var childsNameInput = element(by.id('childs_name'));
-    
-  });
+        //TEST 7
+    it('En el modal "New Child" se puede hace click en el button cancel', function() {
+        addAChild.click();
+        titleModalNewChild = element(by.id('head_New_Child'));
+        expect(titleModalNewChild.getText()).toEqual("New Child");
+        element(by.buttonText("Cancel")).click();
+        browser.sleep(2000);
+        expect(browser.getTitle()).toEqual('Manage Children');
+        browser.sleep(2000);
+    });
 
 
 
-/*
-  it('should add one and two', function() {
-    firstNumber.sendKeys(1);
-    secondNumber.sendKeys(2);
-
-    goButton.click();
-
-    expect(latestResult.getText()).toEqual('3');
-  });
-
-  it('should add four and six', function() {
-    firstNumber.sendKeys(9);
-    secondNumber.sendKeys(1);
-    goButton.click();
-    expect(latestResult.getText()).toEqual('10');
-  });*/
 });
