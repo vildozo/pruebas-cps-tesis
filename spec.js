7ac2a01626387f42a622569a0cad1dfa728a4523
vildozo <vildozo@outlook.com>
2017-12-13 00:29:24 -0400

    pruebas pasando pero comentadas para 1 niño

--- spec.js
+++ spec.js
@@ -1,4 +1,4 @@
-
+var logger = require('./log');
 describe('Protractor Children Management', function() {
 
   var factoryNewChild = element(by.css('title title-left'));
@@ -11,75 +11,114 @@ describe('Protractor Children Management', function() {
     browser.get('http://localhost:8100');
   });
 
-  //       //TEST 1
-  //   it('should have a title', function() {
-  //    expect(browser.getTitle()).toEqual('Manage Children');
-  //  });
-  //
-  //       //TEST 2
-  // it('No Child registered', function() {
-  //   expect(element(by.id('NoChildRegistered')).getText()).toEqual("PLEASE REGISTER ONE ABOVE");
-  // });
 
-        //TEST 3
+//   //TEST 1
+//     it('Open app on the Manage Children View', function() {
+//         expect(browser.getTitle()).toEqual('Manage Children');
+//         logger.log('info','');
+//    });
+//
+// //TEST 2
+//   it('No Child registered', function() {
+//         expect(element(by.id('NoChildRegistered')).getText()).toEqual("PLEASE REGISTER ONE ABOVE");
+//         logger.log('info','');
+//   });
+//
+// //TEST 3
+//     it('Button Create disable when creating a new childs name field is empty', function() {
+//         addAChild.click();
+//         element(by.model("child.gender")).element(by.css("[value='Male']")).click();
+//         var createButton = element(by.buttonText("Create"));
+//         // expect(createButton.isEnabled()).toBe(false);
+//         logger.log('info','Button Create disable when creating a new childs name field is empty');
+//     });
+//
+// //TEST 4
+//     it('Registrar niño con fecha posterior a la actual', function() {
+//         addAChild.click();
+//         var childsNameInput = element(by.id('childs_name'));
+//         element(by.model("child.first_name")).sendKeys("Javier Vildozo");
+//         element(by.model("child.gender")).element(by.css("[value='Male']")).click();
+//         element(by.model("child.birthday")).sendKeys("06/22/2081");
+//         element(by.buttonText("Create")).click();
+//         this.popupContainsHeaderText = function (text) {
+//             this.popupShouldExist();
+//             expect(this.popup.element(by.css('.popup-head')).getText()).toMatch("Please insert a date prior to today");
+//         };
+//         logger.log('info','');
+//     });
+//
+// //TEST 5
+//     it('En el modal "New Child" se puede hace click en el button cancel', function() {
+//         addAChild.click();
+//         titleModalNewChild = element(by.id('head_New_Child'));
+//         expect(titleModalNewChild.getText()).toEqual("New Child");
+//         element(by.buttonText("Cancel")).click();
+//         expect(browser.getTitle()).toEqual('Manage Children');
+//         logger.log('info','');
+//     });
 
-    // it('Open New Child modal', function() {
-    //     addAChild.click();
-    //     titleModalNewChild = element(by.id('head_New_Child'));
-    //     expect(titleModalNewChild.getText()).toEqual("New Child");
-    // });
-
-        //TEST 4 FALLA ESTA PRUEBA
-  // it('Add a Child', function() {
-  //   addAChild.click();
-  //   var childsNameInput = element(by.id('childs_name'));
-  //   element(by.model("child.first_name")).sendKeys("Javier Vildozo");
-  //   element(by.model("child.birthday")).sendKeys("06/22/1981");
-  //   element(by.model("child.gender")).element(by.css("[value='Male']")).click();
-  //   element(by.buttonText("Create")).click();
-  //   browser.sleep(2000);
-  //   element.all(by.id("nombreNino")).getText().then(function (text) {
-  //       console.log(text);
-  //   })
-  //   browser.sleep(10000);
-  // });
-
-        //TEST 5
-    // it('Cant create a child with the name field empty', function() {
-    //     addAChild.click();
-    //     var childsNameInput = element(by.id('childs_name'));
-    //     element(by.model("child.gender")).element(by.css("[value='Male']")).click();
-    //     expect(element(by.id('head_New_Child')).getText()).toEqual("New Child");
-    //
-    //     browser.sleep(1000);
-    // });
-
-    //     //TEST 6
-    // it('Registrar niño con fecha posterior a la actual', function() {
-    //   addAChild.click();
-    //   var childsNameInput = element(by.id('childs_name'));
-    //   element(by.model("child.first_name")).sendKeys("Javier Vildozo");
-    //   element(by.model("child.gender")).element(by.css("[value='Male']")).click();
-    //   element(by.model("child.birthday")).sendKeys("06/22/2081");
-    //   element(by.buttonText("Create")).click();
-    //     this.popupContainsHeaderText = function (text) {
-    //         this.popupShouldExist();
-    //         expect(this.popup.element(by.css('.popup-head')).getText()).toMatch("Please insert a date prior to today");
-    //     };
-    //   browser.sleep(4000);
-    // });
-
-
-        //TEST 7
-    it('En el modal "New Child" se puede hace click en el button cancel', function() {
+//TEST 6
+      it('Add first Child', function() {
         addAChild.click();
-        titleModalNewChild = element(by.id('head_New_Child'));
-        expect(titleModalNewChild.getText()).toEqual("New Child");
-        element(by.buttonText("Cancel")).click();
-        browser.sleep(2000);
-        expect(browser.getTitle()).toEqual('Manage Children');
-        browser.sleep(2000);
+        var childsNameInput = element(by.id('childs_name'));
+        element(by.model("child.first_name")).sendKeys("Javier Vildozo");
+        element(by.model("child.birthday")).sendKeys("06/22/1981");
+        element(by.model("child.gender")).element(by.css("[value='Male']")).click();
+        element(by.buttonText("Create")).click();
+        browser.sleep(8000);
+        
+        element(by.repeater("child in childs")).getText().then(function (text) {
+            expect(text).toMatch("Javier Vildozo");
+            expect(text).toMatch("22/06/81");
+            var mypic = element(by.css("img[src*='boy.png']"));
+            //expect(mypic.isPresent()).toBe(true);
+        });
+
+        logger.log('info','');
     });
+
+//TEST 5
+    it('Edit a Child', function() {
+        var moreButton= element(by.id('moreButton'));
+        addAChild.click();
+        browser.sleep(10000);
+        logger.log('info','');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //TEST 7
+//     it('Crear 2do niño ', function() {
+//           addAChild.click();
+//           var childsNameInput = element(by.id('childs_name'));
+//           element(by.model("child.first_name")).sendKeys("Maria Coloma");
+//           element(by.model("child.gender")).element(by.css("[value='Female']")).click();
+//           element(by.model("child.birthday")).sendKeys("04/12/1980");
+//           element(by.buttonText("Create")).click();
+//
+//         element(by.repeater("child in childs")).getText().then(function (text) {
+//             // expect(text).toMatch("Javier Vildozo");
+//             // expect(text).toMatch("22/06/81");
+//
+//
+//         });
+//
+//           logger.log('info','');
+//     });
+
+
 
 
 
